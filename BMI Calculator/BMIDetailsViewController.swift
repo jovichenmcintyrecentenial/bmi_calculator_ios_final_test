a794662f4e07d28bb89aa74c6cc3fbbed6e70ae0
jovi848 <j.mcintyre@senit.com>
2022-12-15 08:22:27 -0500

    connect BMI viewcontroller to UI

---
+++ BMI Calculator/BMIDetailsViewController.swift
@@ -0,0 +1,31 @@
+//
+//  BMIDetailsViewController.swift
+//  BMI Calculator
+//
+//  Created by Jovi on 15/12/2022.
+//
+
+import UIKit
+
+class BMIDetailsViewController: BMIBaseViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func onDoneAction(_ sender: Any) {
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
